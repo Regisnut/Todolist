da7f05760edfcf6f6d7e0048de64c92806adabea
regis <regis@MacBook-Pro-de-regis.local>
2019-08-04 16:29:57 +0200

    component li

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Todo extends React.Component {
+  render = () => {
+    return (
+      <li>
+        onClick=
+        {() => {
+          this.props.onToClick();
+        }}
+        className={this.props.done === false ? "" : "selected"}><span>X</span>
+        {" " + this.props.title}
+      </li>
+    );
+  };
+}
+
+class App extends React.Component {
+  state = {
+    todos: [
+      { title: "Learn to code", done: true },
+      { title: "Rule the world", done: false }
+    ],
+    inputValue: "Titre"
+  };
+
+  handleClick = () => {
+    const todosCopy = [...this.state.todos]; //on copie todos avant de le modifier
+    todosCopy.push({ title: this.state.inputValue, done: false });
+    this.setState({ todos: todosCopy });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>To-Do list</h1>
+        <ul>
+          {this.state.todos.map((item, index) => {
+            return (
+              <Todo
+                key={index}
+                done={item.done}
+                title={item.title}
+                onToClick={() => {
+                  const changedTodo = { ...this.state.todos[index] };
+                  changedTodo.done = !changedTodo.done;
+                  const changedTodos = [...this.state.todos];
+                  changedTodos[index] = changedTodo;
+                  this.setState({ todos: changedTodos });
+                }}
+              />
+            );
+          })}
+        </ul>
+        <input
+          value={this.state.inputValue}
+          onChange={event => {
+            this.setState({ inputValue: event.target.value });
+          }}
+        />
+        <div>
+          <button
+            onClick={() => {
+              this.handleClick();
+            }}
+          >
+            {"AJOUTER UNE TÀCHE"}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
